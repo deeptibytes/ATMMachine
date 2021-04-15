3ca5fd70b84cffa98f86c5d04f2e01a460e2672b
deepti <deepti@192.168.68.131>
2021-04-15 08:50:14 -0700

    Added comments

--- src/atm/manager/ATMWithdrawlManager.java
+++ src/atm/manager/ATMWithdrawlManager.java
@@ -90,21 +90,29 @@ public class ATMWithdrawlManager implements IATMManager{
 	 
 	 
 }
-
+/*This method with check where the withdrawl amount falls on the DENOM_TREE. 
+ * Then get the closest node in the DENOM_TREE
+ * Retrieve the sorted list (reverse sorted) of smaller denoms from the closest denom node
+ * Loop through the small denoms and if the required number of denominations is available in the repo
+ *
+ */
 
   public  String createDispenseMap(Map<Integer, Integer> currBalanceMap, 
 								  Map<Integer, Integer> dispenseMap, 
 								  int amount){
    
-   DenomNode closestDenom = getClosestDenom(amount, Denomination.DENOM_TREE.getRoot());
+  
+   //If amount is higher than higheset denom, no need of binary search, just retrieve the allDenomSet from DENOM_TREE object
+   //If amount is less than highest, then get closeset node and retrieve list of smaller denoms than node.
+   
    SortedSet<Integer> smallDenomSet = (amount >= Denomination.DENOM_TREE.getMax()) ?
                                       Denomination.DENOM_TREE.getAllDenomSet() : 
-                                      closestDenom.smallDenomSet;
+                                      getClosestDenom(amount, Denomination.DENOM_TREE.getRoot()).smallDenomSet;
                                       System.out.println("smallDenomSet "+smallDenomSet);
-  
+  //Loop through the all the smaller denoms
   for(int denom : smallDenomSet){
     
-    //Base Condition: if amount to be withdrawn is 10 but denom is 50. Invalid denom, pick lower denom
+    //Base Condition: if amount to be withdrawn is 10 but current denom is 50. Invalid denom, pick lower denom
       if(amount < denom){
         continue;
       }
@@ -118,12 +126,12 @@ public class ATMWithdrawlManager implements IATMManager{
          return ATMUtils.WITHDRAWL_FAILED;
       }
       
-       int reqBills = amount/denom;
-       int valueDB = currBalanceMap.get(denom);
+       int reqBills = amount/denom;//calculate how many bills are required for the amount
+       int valueDB = currBalanceMap.get(denom);//check how many available in DB
        int dispenseValue = (valueDB >= reqBills) ? reqBills : valueDB;
        
         dispenseMap.put(denom, dispenseValue ); 
-        amount = amount - denom * dispenseValue;
+        amount = amount - denom * dispenseValue; //check how much amount is left
         
         if(amount == 0)//amount = 0 means transaction complete
 		    return null; 
@@ -134,19 +142,24 @@ public class ATMWithdrawlManager implements IATMManager{
    return (amount != 0) ? ATMUtils.WITHDRAWL_FAILED : null;
  }
  
- 
+ /*
+  * This method with check where the withdrawl amount falls on the DENOM_TREE.. Modification of binary search
+  */
  private DenomNode getClosestDenom(int amount, DenomNode root){
    
    //Base Cases - start
    if(root == null) return null;
-   
-   if(amount == root.denomValue){
+
+//if amount is equal to one of the denoms. For example, of amount is 20, we take closest node as 50.Node 50 has small denom list {20, 20, 5, 1)
+//so for amount 20, the list of thse denoms {20, 20, 5, 1) will be considered   
+   if(amount == root.denomValue){  
        return root.right;
-   
+
+//If amount is less than root and between 10 and 5 (eg..7), closest node will be 10. So for amount 7, the list of these denoms {5, 1) will be considered   
    }else if(root.left != null && 
             amount < root.denomValue && amount > root.left.denomValue){
      return root.left;
-   
+//If amount is more than root and between 10 and 20 (eg..15), closest node will be 20. So for amount 15, the list of these denoms {10, 5, 1) will be considered      
    } else if(root.right != null && 
              amount > root.denomValue && amount < root.right.denomValue){
   
@@ -154,7 +167,7 @@ public class ATMWithdrawlManager implements IATMManager{
    }
    //Base Cases - end
    
-   
+   //Recursive call
    if(amount < root.denomValue){
      return getClosestDenom(amount, root.left);
    }  
